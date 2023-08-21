dc75483ab889d9f731ddfa1f2eb47263c4a6400b
özge kilic <ozgeklc096@gmail.com>
2023-08-21 17:57:49 +0300

    controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_141000) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_130656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "start_date"
+    t.integer "return_date"
+    t.integer "total_price"
+    t.string "confirmation"
+    t.bigint "user_id", null: false
+    t.bigint "robot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["robot_id"], name: "index_bookings_on_robot_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "robots", force: :cascade do |t|
+    t.string "robot_name"
+    t.string "category"
+    t.string "description"
+    t.integer "price"
+    t.string "robot_pictures"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_robots_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_141000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "robots"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "robots", "users"
 end
