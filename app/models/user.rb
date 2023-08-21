5cbb3b3686e8eefecdf256c3bb8c9d871c7615bd
Giovane Nery <48683509+GiovaneNery@users.noreply.github.com>
2023-08-21 17:00:14 +0200

    Revert "home html change"

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, :password, :address, presence: true
-  validates :password, length: { minimum: 6 }
-end
